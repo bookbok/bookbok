c5538d41cee740259c3d326604d6c77ea3aa4064
kento-oka <kento-oka@kentoka.com>
2021-06-17 20:48:34 +0900

    命名とかいろいろ

--- app/Services/Webpack.php
+++ app/Services/Webpack.php
@@ -3,50 +3,32 @@ namespace App\Services;
 
 class Webpack
 {
-    /**
-     * @var string
-     */
-    private $manifest_path;
+    private string $manifestPath;
 
     /**
      * manifest.jsonから取得したエントリーポイント名と実ファイル名のマップ
-     * @var array<string,string>|null
+     * @phpstan-var array<string,string>|null
      */
-    private $manifest;
+    private ?array $manifest;
 
-    /**
-     * @param string $public_path
-     * @param string $build_file_dir_name
-     * @param string $manifest_filename
-     * @return void
-     */
     public function __construct(
-        string $public_path,
-        string $build_file_dir_name,
-        string $manifest_filename
+        string $publicPath,
+        string $buildFileDirName,
+        string $manifestFilename
     ) {
-        $this->manifest_path = implode(DIRECTORY_SEPARATOR, [$public_path, $build_file_dir_name, $manifest_filename]);
+        $this->manifestPath = implode(DIRECTORY_SEPARATOR, [$publicPath, $buildFileDirName, $manifestFilename]);
     }
 
     /**
      * manifest.jsonからファイル名のマップを読み込む。
      */
     private function load(): void {
-        if ($this->manifest !== null) {
-            return;
-        }
-
-        $manifest = file_get_contents($this->manifest_path);
+        $manifest = file_get_contents($this->manifestPath);
         if ($manifest === false) {
             throw new \Exception('ビルドしていない');
         }
 
-        try {
-            $this->manifest = json_decode($manifest, true);
-        } catch (\Throwable $e) {
-            $this->manifest = null;
-            throw new \Exception('マニフェストをjsonパースできなかった', 0, $e);
-        }
+        $this->manifest = json_decode($manifest, true);
     }
 
     /**
@@ -56,7 +38,9 @@ class Webpack
      * @return string
      */
     public function get(string $name): string {
-        $this->load();
+        if ($this->manifest === null) {
+            $this->load();
+        }
 
         if (!isset($this->manifest[$name])) {
             throw new \Exception("マニフェスト存在しないエントリーポイント名が指定された（{$name}）");
