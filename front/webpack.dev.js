36b22c2a794ded753939acff612389def9cd3c51
kento-oka <kento-oka@kentoka.com>
2021-07-03 22:20:55 +0900

    ソースマップ出すように

--- front/webpack.dev.js
+++ front/webpack.dev.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const { merge } = require('webpack-merge')
-const base = require('./webpack.base.js')
+const { merge } = require('webpack-merge');
+const base = require('./webpack.base.js');
 
 const config = merge(base, {
   mode: 'development',
-})
+  devtool: 'source-map',
+});
 
-module.exports = config
+module.exports = config;
