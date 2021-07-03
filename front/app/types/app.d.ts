c01aabbf87564fcfb1855ae9d7299a3e86d5a505
kento-oka <kento-oka@kentoka.com>
2021-07-03 22:27:59 +0900

    必要な型定義を追加

---
+++ front/app/types/app.d.ts
@@ -0,0 +1,4 @@
+/** 型を書くのが面倒だけど後から書く気がある場合に使用する */
+type TODO = any; // eslint-disable-line @typescript-eslint/no-explicit-any
+/** 意図的にanyを使う場合に使用する */
+type KNOW = any; // eslint-disable-line @typescript-eslint/no-explicit-any
